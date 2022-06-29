40288ec8a229cdf468aa7c777c948de85c0edd8c
ch-4ml <fetch@ajou.ac.kr>
2022-06-29 12:00:31 +0900

    fix: Fix path

--- application-simple/up.sh
+++ application-simple/up.sh
@@ -2,6 +2,6 @@ pushd ../../test-network
 
 ./network.sh up createChannel -c mychannel -ca -s couchdb
 
-./network.sh deployCC -ccs 1 -ccv 1 -ccep "OR('Org1MSP.peer','Org2MSP.peer')"  -ccl go -ccp ../../asset-transfer-events/chaincode-simple-go/ -ccn asset-transfer-events-simple-go -cci InitAssetCounter
+./network.sh deployCC -ccs 1 -ccv 1 -ccep "OR('Org1MSP.peer','Org2MSP.peer')"  -ccl go -ccp ../asset-transfer-events/chaincode-simple-go/ -ccn asset-transfer-events-simple-go -cci InitAssetCounter
 
 popd
